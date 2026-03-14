f3809db824cbf0b86b8da9663c3cd9d7b72c97eb
unknown <yimingsu2007@gmail.com>
2026-03-14 12:34:14 -0700

    add home panel

---
+++ src/panels/HomeRight.jsx
@@ -0,0 +1,30 @@
+import TypedText from '../components/TypedText'
+import useUntypeTracker from '../components/useUntypeTracker'
+
+function HomeRight({ visible, onNavigate, onUntypeComplete }) {
+  const onUntyped = useUntypeTracker(5, visible, onUntypeComplete)
+
+  return (
+    <>
+      <div id="name-title" onClick={() => onNavigate('home')}>
+        <TypedText text="Yiming Su" visible={visible} speed={25} onUntyped={onUntyped} untypeFrom="start" />
+      </div>
+      <nav id="menu">
+        <a onClick={() => onNavigate('about')}>
+          <TypedText text="About Me" icon="user" visible={visible} speed={25} onUntyped={onUntyped} untypeFrom="start" />
+        </a>
+        <a onClick={() => onNavigate('experience')}>
+          <TypedText text="Experience" icon="trophy" visible={visible} speed={25} onUntyped={onUntyped} untypeFrom="start" />
+        </a>
+        <a onClick={() => onNavigate('projects')}>
+          <TypedText text="Projects" icon="code" visible={visible} speed={25} onUntyped={onUntyped} untypeFrom="start" />
+        </a>
+        <a onClick={() => onNavigate('contact')}>
+          <TypedText text="Contact Me" icon="envelope" visible={visible} speed={25} onUntyped={onUntyped} untypeFrom="start" />
+        </a>
+      </nav>
+    </>
+  )
+}
+
+export default HomeRight
