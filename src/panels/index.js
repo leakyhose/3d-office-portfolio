25304a8f77387930a92100b6c64b564d917c2024
unknown <yimingsu2007@gmail.com>
2026-03-15 20:23:54 -0700

    register about and experience panels

--- src/panels/index.js
+++ src/panels/index.js
@@ -1,7 +1,11 @@
 import HomeRight from './HomeRight'
+import AboutLeft from './AboutLeft'
+import ExperienceLeft from './ExperienceLeft'
 
 const PANELS = {
   'home': HomeRight,
+  'about': AboutLeft,
+  'experience': ExperienceLeft,
 }
 
 export default PANELS
