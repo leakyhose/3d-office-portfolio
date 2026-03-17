716e57b0cf29c81693e2c5905e22175a59b41085
unknown <yimingsu2007@gmail.com>
2026-03-17 16:48:43 -0700

    add projects panels

---
+++ src/panels/ProjectsLeft.jsx
@@ -0,0 +1,25 @@
+import TypedText from '../components/TypedText'
+import useUntypeTracker from '../components/useUntypeTracker'
+
+function ProjectsLeft({ visible, onNavigate, onUntypeComplete }) {
+  const onUntyped = useUntypeTracker(4, visible, onUntypeComplete)
+
+  return (
+    <nav className="projects-nav">
+      <a onClick={() => onNavigate('home')}>
+        <TypedText text="Home" icon="home" visible={visible} speed={23} onUntyped={onUntyped} />
+      </a>
+      <a onClick={() => onNavigate('about')}>
+        <TypedText text="About" icon="user" visible={visible} speed={23} onUntyped={onUntyped} />
+      </a>
+      <a onClick={() => onNavigate('experience')}>
+        <TypedText text="Experience" icon="trophy" visible={visible} speed={23} onUntyped={onUntyped} />
+      </a>
+      <a onClick={() => onNavigate('contact')}>
+        <TypedText text="Contact" icon="envelope" visible={visible} speed={23} onUntyped={onUntyped} />
+      </a>
+    </nav>
+  )
+}
+
+export default ProjectsLeft
---
+++ src/panels/ProjectsRight.jsx
@@ -0,0 +1,19 @@
+import TypedText from '../components/TypedText'
+import useUntypeTracker from '../components/useUntypeTracker'
+
+function ProjectsRight({ visible, onNavigate, onUntypeComplete }) {
+  const onUntyped = useUntypeTracker(2, visible, onUntypeComplete)
+
+  return (
+    <div id="projects-panel" className="visible">
+      <h1 className="panel-title">
+        <TypedText text="Projects" icon="folder-open" visible={visible} speed={35} reserveSpace={true} onUntyped={onUntyped} untypeFrom="start" />
+      </h1>
+      <p className="panel-line">
+        <TypedText text="Coming soon..." visible={visible} speed={20} reserveSpace={true} onUntyped={onUntyped} untypeFrom="start" />
+      </p>
+    </div>
+  )
+}
+
+export default ProjectsRight
