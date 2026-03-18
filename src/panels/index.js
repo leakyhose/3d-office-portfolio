d4a84685539f016726a2ae20f3b8f581cd7493b1
unknown <yimingsu2007@gmail.com>
2026-03-18 11:47:38 -0700

    register all panels

--- src/panels/index.js
+++ src/panels/index.js
@@ -1,11 +1,19 @@
 import HomeRight from './HomeRight'
 import AboutLeft from './AboutLeft'
+import ContactLeft from './ContactLeft'
+import ContactRight from './ContactRight'
 import ExperienceLeft from './ExperienceLeft'
+import ProjectsLeft from './ProjectsLeft'
+import ProjectsRight from './ProjectsRight'
 
 const PANELS = {
   'home': HomeRight,
   'about': AboutLeft,
+  'contact-left': ContactLeft,
+  'contact-right': ContactRight,
   'experience': ExperienceLeft,
+  'projects-nav': ProjectsLeft,
+  'projects': ProjectsRight,
 }
 
 export default PANELS
